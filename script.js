8a27e432aec21d70d323ec30a13468b9ded0ba23
Martin Maul <martin.maul@hs-weingarten.de>
2021-09-18 14:50:49 +0200

    done just cleaning todo

--- script.js
+++ script.js
@@ -5,18 +5,6 @@ const restartButton = document.querySelector(".restart");
 const player1NameInput = document.querySelector("#player1NameInput");
 const player2NameInput = document.querySelector("#player2NameInput");
 
-let numberOfTurns = 0;
-const winningCombos = 
-[[1,2,3],
- [4,5,6],
- [7,8,9],
- [1,4,7],
- [2,5,8],
- [3,6,9],
- [1,5,9],
- [3,5,7]
-];
-
 //GameBoard Module
 const gameBoard = (() => {
     let board = ["","","","","","","","",""];
@@ -47,6 +35,17 @@ const gameBoard = (() => {
 
 //DisplayController Module
 const displayController = (() => {
+    let numberOfTurns = 0;
+    const winningCombos = 
+        [[1,2,3],
+        [4,5,6],
+        [7,8,9],
+        [1,4,7],
+        [2,5,8],
+        [3,6,9],
+        [1,5,9],
+        [3,5,7]
+        ];
 
     let currentPlayer = null;
 
@@ -57,7 +56,7 @@ const displayController = (() => {
 
     const startGame = () => {
         boardGrid.style.opacity = "1";
-        //setting Playername if not empty
+
         if(player1NameInput.value == ""){
             player1.setName("Player 1");
         } else {
@@ -79,21 +78,17 @@ const displayController = (() => {
         });
     }
     
-
-
     const displayMove = (e) => {
         //set Sign in box of currentPlayer
         if(e.target.innerHTML != ""){
             alert("Box already taken!");
         } else {
             e.target.innerHTML = displayController.currentPlayer.getSign();
-            numberOfTurns++;
+            displayController.numberOfTurns++;
             gameBoard.updateBoardBox(e);
             checkBoardStatus();
             switchCurrentPlayer();
         }
-        
-
     }
 
     const switchCurrentPlayer = () => {
@@ -108,19 +103,16 @@ const displayController = (() => {
     const checkBoardStatus = () => {
         let oPositions = [];
         let xPositions = [];
-        let winner = "";
+        let winner = false;
         
         // store player's turns
         for(let i = 0; i < gameBoard.getBoard().length; i++) {
-
             if(gameBoard.getBoard()[i] == "X") {
                 xPositions.push(i+1);
             } 
-
             if(gameBoard.getBoard()[i] == "O"){
                 oPositions.push(i+1);
             }
-
         }
         
         // check with winning Combo if someone won
@@ -129,42 +121,40 @@ const displayController = (() => {
         for(let w = 0; w < winningCombos.length; w++){
 
             if(checker(xPositions, winningCombos[w]) && xPositions.length >= 3){
-                
                 setTimeout(function(){alert(`${player1.getName()} HAS WON`)},100);
                 setTimeout(function(){restartGame();},3000);
-                winner = displayController.currentPlayer.getName();
-                
-                
+                winner = true;
             }
             if(checker(oPositions, winningCombos[w]) && oPositions.length >= 3){
                 setTimeout(function(){alert(`${player2.getName()} HAS WON`)},100);
                 setTimeout(function(){restartGame();},3000);
-                winner = displayController.currentPlayer.getName();
-                
+                winner = true;
             }
         }
         
-        if(numberOfTurns < 9){
+        if(displayController.numberOfTurns < 9){
             return;
-        } else if (numberOfTurns >= 9 && winner == "") {
+        } else if (displayController.numberOfTurns >= 9 && !winner) {
             setTimeout(function(){alert("Game is Over! Tie Match!")},100);
             setTimeout(function(){restartGame();},3000);
-            
         } 
-        
     }
+    
     const restartGame = () => {
         boardGrid.innerHTML = "";
         gameBoard.resetBoard();
         displayController.currentPlayer = player1;
-        numberOfTurns = 0;
+        displayController.numberOfTurns = 0;
+        boardGrid.style.opacity = "0.4";
         setupGame();
         startGame();
-    }   
+    }
 
+    startButton.addEventListener("click", startGame);
+    restartButton.addEventListener("click", restartGame);
 
     return {
-     displayMove, setupGame, startGame, currentPlayer, restartGame
+     displayMove, setupGame, startGame, currentPlayer, restartGame, numberOfTurns
     };
 })();
 
@@ -183,23 +173,17 @@ const player1 = Player("me","X");
 const player2 = Player("cpu","O");
 
 
-
-startButton.addEventListener("click", displayController.startGame);
-restartButton.addEventListener("click", displayController.restartGame);
-
-
 // DONE LOGIC Function um zu schauen ob schon jemand gewonnen hat
 
 
 //eventlistener beim grid in startGame rein
 
 // TODO
-// Gewinnermeldung
-// Spielername eingeben, Sign eingeben
-// Buttons (Reset)
-// Design
+// DONE Gewinnermeldung
+// DONE Spielername eingeben, Sign eingeben
+// DONE Buttons (Reset)
+// DONE Design
+// Checken wie sich andere executen (wenn alles in factory/ modules sind) // einfach alles über startbutton event
+// demensprechend weiter versuchen alles zu verpacken // Players auch in start game
+// Verantwortlichkeiten nochmal checken
 
-// CHECK
-// Globals verhindern
-// So gut wie möglich einpacke in Module / Factory
-// Verantwortlichkeiten dementsprechend zufügen
